d96bc3e3dcdb5d0bfc2a83fcf784a2b901230bba
Juan Arias <juanmanuel0560@gmail.com>
2022-02-25 13:26:10 -0500

    Users show all

--- main.py
+++ main.py
@@ -95,8 +95,6 @@ def signup(user:UserRegister = Body(...)):
         f.write(json.dumps(results))
         return user
 
-
-
 ### Login a User
 @app.post(
     path="/login",
@@ -117,7 +115,21 @@ def login():
     tags=["Users"]
 )
 def show_all_user():
-    pass
+    """This path operation shows all users in the app    
+
+    Parameters:
+        -
+
+    Returns a json list with all users un the app,with the following keys:
+    - user_id    : UUID
+    - email      : Emailstr
+    - first_name : str
+    - last_name  : str
+    - birth_date : date
+    """
+    with open("users.json","r",encoding="utf-8")as f:
+        results=json.loads(f.read())
+        return results
 
 ### Show a user 
 @app.get(
